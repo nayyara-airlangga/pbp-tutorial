c5def24c3203bcd647ca8df9ef4ccd2a196d7271
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-09-07 14:33:18 +0700

    Render wishlist data in wishlist view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from wishlist.models import BarangWishList
 
 # Create your views here.
 def show_wishlist(request):
-    return render(request, "wishlist.html")
+    data_barang_wishlist = BarangWishList.objects.all()
+    context = {'list_barang': data_barang_wishlist, 'nama': 'Nayyara Airlangga Raharjo'}
+
+    return render(request, 'wishlist.html', context)
