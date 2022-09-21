88ece6442159d0c2c66902f2dcbc40d02741f456
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-09-22 00:48:45 +0700

    Create logout view

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from wishlist.views import (
     login_user,
+    logout_user,
     register,
     show_wishlist,
     show_wishlist_xml,
@@ -15,6 +16,7 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('xml/', show_wishlist_xml, name='show_wishlist_xml'),
     path('json/', show_wishlist_json, name='show_wishlist_json'),
     path('xml/<int:id>/', show_wishlist_xml_by_id, name='show_wishlist_xml_by_id'),
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
 from django.http import HttpResponse
@@ -33,6 +33,11 @@ def login_user(request):
             messages.info(request, 'Username atau Password salah!')
     context = {}
     return render(request, 'login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('wishlist:login')
 
 
 def show_wishlist(request):
