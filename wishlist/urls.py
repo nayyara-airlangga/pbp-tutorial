205aa139abcd1cf4ab8a6b47f3a0de5153e408af
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-09-22 00:46:00 +0700

    Add login handler

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from wishlist.views import (
+    login_user,
     register,
     show_wishlist,
     show_wishlist_xml,
@@ -13,6 +14,7 @@ app_name = 'wishlist'
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
     path('xml/', show_wishlist_xml, name='show_wishlist_xml'),
     path('json/', show_wishlist_json, name='show_wishlist_json'),
     path('xml/<int:id>/', show_wishlist_xml_by_id, name='show_wishlist_xml_by_id'),
